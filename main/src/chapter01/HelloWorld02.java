3ac8464b6ba12bc7f2776b06a33dce78d6664022
hzauggj <279103446@qq.com>
2022-01-01 22:04:25 +0800

    程序员小猪的第三个程序

---
+++ main/src/chapter01/HelloWorld02.java
@@ -0,0 +1,14 @@
+package chapter01;
+
+/**
+ * @Description
+ * @Author 程序员小猪
+ * @Version V1.0.0
+ * @Since 1.0
+ * @Date 2022/1/1 0001
+ */
+public class HelloWorld02 {
+    public static void main(String []args){
+        System.out.println("程序员小猪的 第二个HelloWorld 程序");
+    }
+}
